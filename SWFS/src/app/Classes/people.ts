98a81eda97784c876865d038e36a66c925124a1e
Mathew <seuzrulz@gmail.com>
2019-07-16 03:31:05 +0200

    Http get methods figured out

---
+++ SWFS/src/app/Classes/people.ts
@@ -0,0 +1,25 @@
+export class People {
+
+    name: string;
+    height: string;
+    mass: string;
+    // tslint:disable-next-line: variable-name
+    hair_color: string;
+    // tslint:disable-next-line: variable-name
+    skin_color: string;
+    // tslint:disable-next-line: variable-name
+    eye_color: string;
+    // tslint:disable-next-line: variable-name
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: [];
+    species: [];
+    vehicles: [];
+    starships: [];
+    create: string;
+    edited: string;
+    url: string;
+
+
+}
--- SWFS/src/app/app.component.ts
+++ SWFS/src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { People } from './Classes/people';
+import { Http, Response, RequestMethod, RequestOptions, Headers } from '@angular/http';
+import { async } from '@angular/core/testing';
+
+interface getinter {
+  count: number;
+  next: string;
+  previous: string;
+  results: [];
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SWFS';
+  people: [];
+
+  constructor(private http: Http) { }
+
+   ngOnInit() {
+     this.getPeople();
+    console.log(this.people);
+  }
+
+  async getPeople() {
+    console.log(this.people);
+    await this.http.get('https://swapi.co/api/people')
+      .subscribe((res: Response) => {
+        console.log(res);
+        console.log(res.json());
+        console.log(res.json().results);
+        this.people = res.json().results;
+        console.table(this.people);
+       
+      });
+    console.table(this.people);
+  }
 }
+
+
